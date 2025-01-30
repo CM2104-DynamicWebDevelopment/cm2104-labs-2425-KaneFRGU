26afec4781f923bbf608ab2233259909cbed61e4
KaneFRGU <k.farquharson4@rgu.ac.uk>
2025-01-30 11:10:44 +0000

    2

--- lab_11/server.js
+++ lab_11/server.js
@@ -14,4 +14,10 @@ app.get('/joke', function(req, res){
     res.send(randomJoke);
 });
 
+app.get('/add', function(req, res){
+    var x = parseInt(req.query.x);
+    var y = parseInt(req.query.y);
+    res.send("X + Y="+(x+y));
+    });
+
 app.listen(8080);

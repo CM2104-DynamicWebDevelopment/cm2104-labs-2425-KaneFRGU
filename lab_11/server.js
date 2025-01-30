ee634b4594afcfcf5052fd4089ccacd209dd5228
KaneFRGU <k.farquharson4@rgu.ac.uk>
2025-01-30 10:52:06 +0000

    1

--- lab_11/server.js
+++ lab_11/server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 app.get('/', function(req, res){
-res.send("Hello World! from express");
+    res.send("Hello World! from express");
+});
+app.get('/test', function(req, res){
+    res.send("this is route 2");
 });
 app.listen(8080);

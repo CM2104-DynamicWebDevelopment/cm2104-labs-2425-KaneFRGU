e496e190016ac0da7bd9b3b5ef0d04ce17b0a511
KaneFRGU <k.farquharson4@rgu.ac.uk>
2025-01-30 11:15:34 +0000

    2

--- lab_11/server.js
+++ lab_11/server.js
@@ -5,6 +5,13 @@ var knockknock = require('knock-knock-jokes');
 
 app.use(express.static('public'))
 
+app.get('/getform', function(req, res){
+    var name = req.query.name;
+    var quest = req.query.quest;
+     res.send("Hi "+name+" I am sure you will "+quest) ;
+    });
+    
+
 app.get('/', function(req, res){
 res.send("Hello World! from express");
 });

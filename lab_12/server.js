e64567d11c69f886dae3184d0e1f5c14f2b61318
KaneFRGU <k.farquharson4@rgu.ac.uk>
2025-02-06 11:40:44 +0000

    dumb

--- lab_12/server.js
+++ lab_12/server.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var app = express();
 var SpotifyWebApi = require('spotify-web-api-node')
-var searchterm = "love";
 app.use(express.static('public'))
 
 var spotifyApi = new SpotifyWebApi({
@@ -12,11 +11,10 @@ var spotifyApi = new SpotifyWebApi({
 // Retrieve an access token
 spotifyApi.clientCredentialsGrant().then( 
     function (data) {
-        spotifyApi.setAccessToken(data.body['access_token']); 
         console.log('The access token expires in ' + data.body['expires_in']);
         console.log('The access token is ' + data.body['access_token']);
         // Save the access token so that it's used in future calls
-        
+        spotifyApi.setAccessToken(data.body['access_token']); 
 
         console.log('The access token is :) ')
     },
@@ -29,33 +27,33 @@ spotifyApi.clientCredentialsGrant().then(
 );
 
 
-
-spotifyApi.searchTracks(searchterm)
-   .then(function (data) {
-   var tracks = data.body.tracks.items 
-   //lets set up a empty string to act as the response
-   var HTMLResponse = "";
-   //now lets run through all the items
-   //this is a for loop 
-   for(var i=0; i<tracks.length;i++){
-   
-   var track = tracks[i];
-   console.log(track.name);
-   HTMLResponse = HTMLResponse +
-   "<div>" +
-   "<h2>"+track.name+"</h2>"+
-   "<h4>"+track.artists[0].name+"</h4>"+
-   "<img src='"+track.album.images[0].url +"'>"+
-   "<a href='"+track.external_urls.spotify+"'> Track Details </a>"+ "</div>";
-   console.log(HTMLResponse);
-   }
-   res.send(HTMLResponse)
-   }, function (err) {
-    console.error(err);
-   });
-  
-   
-
+async function getTracks(searchterm, res){
+    spotifyApi.searchTracks(searchterm)
+    .then(function (data) {
+    var tracks = data.body.tracks.items 
+    //lets set up a empty string to act as the response
+    var HTMLResponse = "";
+    //now lets run through all the items
+    //this is a for loop 
+    for(var i=0; i<tracks.length;i++){
+    
+    var track = tracks[i];
+    console.log(track.name);
+    HTMLResponse = HTMLResponse +
+    "<div>" +
+    "<h2>"+track.name+"</h2>"+
+    "<h4>"+track.artists[0].name+"</h4>"+
+    "<img src='"+track.album.images[0].url +"'>"+
+    "<a href='"+track.external_urls.spotify+"'> Track Details </a>"+ "</div>";
+    console.log(HTMLResponse);
+    }
+    res.send(HTMLResponse)
+    }, function (err) {
+        console.error(err);
+    });
+    
+    
+}
 app.get('/searchLove', function (req, res) {
     getTracks('love', res);
 })

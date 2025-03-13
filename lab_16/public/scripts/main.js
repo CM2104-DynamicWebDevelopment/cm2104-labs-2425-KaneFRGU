46437342b967a2aaa3998d737684fa1886a7f610
KaneFRGU <k.farquharson4@rgu.ac.uk>
2025-03-13 10:31:12 +0000

    1

--- lab_16/public/scripts/main.js
+++ lab_16/public/scripts/main.js
@@ -1,1 +1,11 @@
 var socket = io();
+
+$('#form').submit(function () {
+    var message = $('#input').val();
+    if (message) {
+    socket.emit('chat message', message);
+    $("#input").val("");
+    }
+    return false;
+    })
+   

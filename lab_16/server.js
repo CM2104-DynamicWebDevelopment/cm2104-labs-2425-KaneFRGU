8653d67b8363eb51d60bcde9c5d17314747514a4
KaneFRGU <k.farquharson4@rgu.ac.uk>
2025-03-13 10:32:24 +0000

    1

--- lab_16/server.js
+++ lab_16/server.js
@@ -21,4 +21,15 @@ io.on('connection', function (socket) {
     socket.on('disconnect', function () {
     console.log('user disconnected');
     });
-   })
+   });
+
+io.on('connection', function (socket) {
+console.log('a user connected');
+socket.on('disconnect', function () {
+console.log('user disconnected');
+});
+
+socket.on('chat message', function (msg) {
+console.log('message: ' + msg);
+});
+});
